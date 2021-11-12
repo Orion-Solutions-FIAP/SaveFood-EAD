5a73859d87a728fb685ea7018e52fb5d05d1fef0
kmitsuishi-08 <kelly.mitsuishi@gmail.com>
2021-11-11 23:09:26 -0300

    Criando tela de cadastro de usuário.

---
+++ SaveFood/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SaveFood.Models
+{
+    public class User
+    {
+        [Display(Name="Nome")]
+        public string Name { get; set; }
+        
+        [Display(Name="E-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Display(Name="Senha")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name="Confirme a senha")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
