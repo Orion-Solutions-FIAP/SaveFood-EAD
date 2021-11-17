9db78879e9bee0085b831ce32d77ef1e35cf2bdf
kmitsuishi-08 <kelly.mitsuishi@gmail.com>
2021-11-16 23:53:28 -0300

    Adicionando link de criação de armazenamento e categoria.

--- SaveFood/Controllers/LoginController.cs
+++ SaveFood/Controllers/LoginController.cs
@@ -18,6 +18,8 @@ namespace SaveFood.Controllers
 
         public IActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Home");
             return View();
         }
 
